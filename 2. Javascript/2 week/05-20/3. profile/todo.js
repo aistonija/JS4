f709f1ef725bc1494b3f461c6b5abc1b0a10ce92
aistonija <aistonija16@gmail.com>
2021-05-20 14:02:30 +0300

    JS - submit form -> print card from inputs value - task solution

---
+++ 2. Javascript/2 week/05-20/3. profile/todo.js
@@ -0,0 +1,47 @@
+const form = document.querySelector("form");
+const card = document.querySelector(".card");
+
+form.addEventListener("submit", printCard);
+
+function printCard(event) {
+  event.preventDefault();
+  const imgUrl = event.target.elements.imgUrl.value;
+  const fName = event.target.elements.fName.value;
+  const age = event.target.elements.age.value;
+  const gender = event.target.elements.gender.value;
+  const text = event.target.elements.aboutMeMessage.value;
+  const selection = event.target.elements.relationship.value;
+
+  document.querySelector(".myPhoto").src = imgUrl;
+  document.querySelector(".myName").textContent = fName;
+  document.querySelector(".myAge").textContent = age;
+  document.querySelector(".myGender").textContent = gender;
+  document.querySelector(".aboutMe").textContent = text;
+  document.querySelector(".myStatus").textContent = switchResult(
+    Number(selection)
+  );
+  card.style.display = "block";
+}
+
+function switchResult(options) {
+  let option = "";
+
+  switch (options) {
+    case 1: {
+      option = "Single and ready to Mingle";
+      break;
+    }
+    case 2: {
+      option = "Happily Married";
+      break;
+    }
+    case 3: {
+      option = "I'd rather not say";
+      break;
+    }
+    default:
+      option = "Please choose one of the following";
+  }
+
+  return option;
+}
